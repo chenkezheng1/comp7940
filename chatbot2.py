afef94a2abd9f95ca34dd8d07ea55484675683f9
chenkezheng <chenkezheng@baijiahulian.com>
2024-03-13 17:33:01 +0800

    lab6

--- chatbot2.py
+++ chatbot2.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
 CallbackContext)
-import configparser
+import os
+# import configparser
 import logging
 import redis
 from ChatGPT_HKBU import HKBU_ChatGPT
@@ -9,15 +10,16 @@ from ChatGPT_HKBU import HKBU_ChatGPT
 global redis1
 def main():
     # Load your token and create an Updater for your Bot
-    config = configparser.ConfigParser()
-    config.read('config.ini')
-    updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    # config = configparser.ConfigParser()
+    # config.read('config.ini')
+    # updater = Updater(token=(config['TELEGRAM']['ACCESS_TOKEN']), use_context=True)
+    updater=Updater(token=(os.environ['ACCESS_TOKEN']), use_context=True)
     dispatcher = updater.dispatcher
 
     global redis1
-    redis1 = redis.Redis(host=(config['REDIS']['HOST']),
-        password=(config['REDIS']['PASSWORD']),
-        port=(config['REDIS']['REDISPORT']))
+    redis1 = redis.Redis(host=(os.environ['HOST']),
+        password=(os.environ['PASSWORD']),
+        port=(os.environ['REDISPORT']))
 
     
     # You can set this logging module, so you will know when
@@ -29,7 +31,7 @@ def main():
     # dispatcher.add_handler(echo_handler)
     # dispatcher for chatgpt
     global chatgpt
-    chatgpt = HKBU_ChatGPT(config)
+    chatgpt = HKBU_ChatGPT()
     chatgpt_handler = MessageHandler(Filters.text & (~Filters.command),equiped_chatgpt)
     dispatcher.add_handler(chatgpt_handler)
     
